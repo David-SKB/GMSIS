bb9232e076dce119a09fe8cfbd2afaffdc160cb4
do302 <do302@ITL025.student.eecs.qmul.ac.uk>
2017-01-17 10:41:21 +0000

    adding repairs package

---
+++ GMSIS/src/repairs/ComponentLoader.java
@@ -0,0 +1,15 @@
+
+package repairs;
+
+import java.swing.JOptionPane;
+
+//@author David Okeke
+
+
+public class ComponentLoader 
+{
+    public static void main (String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "The application works.");
+    }
+}
