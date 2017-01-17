5bf010539fa7fdb2e527bd9a1bd58d5c4fddc7f0
joshuascott <joshuascott@25.84.102.169>
2017-01-17 10:34:27 +0000

    System.out.println("Adding <Vehicle Record> package");

---
+++ GMSIS/src/VehicleRecord/ComponentLoader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VehicleRecord;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author joshuascott
+ */
+public class ComponentLoader {
+  public static void main(String[] args){
+   JOPtionPane.showMessageDialog(null, "The application works");   
+  }
+  }  
+}
