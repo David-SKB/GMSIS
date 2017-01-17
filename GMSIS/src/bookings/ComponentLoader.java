ca5856e114390745398d4448a99cf86f9c785103
dja30 <dja30@itl023.student.eecs.qmul.ac.uk>
2017-01-17 10:21:31 +0000

    Adding Diagnosis and Repair Bookings package.

---
+++ GMSIS/src/bookings/ComponentLoader.java
@@ -0,0 +1,8 @@
+package bookings;
+import javax.swing.JOptionPane;
+
+public class ComponentLoader {
+    public static void main(String[] args){
+        JOptionPAne.showMessageDialog(null, "This works!");
+    }
+} 
