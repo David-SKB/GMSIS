faadc2466fe16308d0043abced53b08d3ae77329
Athanasios Gkanos <a.gkanos@se15.qmul.ac.uk>
2017-01-18 22:35:53 +0000

    Customer Account module

---
+++ scratch/Athan/CustomerAccount/src/customerAccount/ComponentLoader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customerAccount;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author athanasiosgkanos
+ */
+public class ComponentLoader {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "The appliction works.");
+    }
+}
